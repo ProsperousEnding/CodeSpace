dda35d8efd9d3e4c99e6cd6646b6cb22af80e0ae
ProsperousEnding <2105642835@qq.com>
2022-08-05 01:39:25 +0800

    项目框架基本搭建

--- code-space/src/test/java/com/code/codespace/CodeSpaceApplicationTests.java
+++ code-space/src/test/java/com/code/codespace/CodeSpaceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodespaceApplicationTests {
+class CodeSpaceApplicationTests {
 
     @Test
     void contextLoads() {
